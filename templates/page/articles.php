825c8db35bd5e747af3855c5dda67d4ad28ed975
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2025-02-18 15:45:42 +0100

    Creation page articles

---
+++ templates/page/articles.php
@@ -0,0 +1,5 @@
+<?php require_once _ROOTPATH_ . '/templates/header.php'; ?>
+
+<?php require_once _ROOTPATH_ . '/templates/article/list.php'; ?>
+
+<?php require_once _ROOTPATH_ . '/templates/footer.php'; ?>
